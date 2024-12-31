98f92124481c026abbf3daa695d8580648309bf8
b-yuko <yuko.banno.oo@gmail.com>
2024-12-31 17:16:18 +0900

    feat: Triple-quoted strings

--- Introduction/Triple-quoted strings/src/Task.kt
+++ Introduction/Triple-quoted strings/src/Task.kt
@@ -1,9 +1,19 @@
+/*
+
+Triple-quoted strings
+
+Kotlinのさまざまな文字列リテラルおよび文字列テンプレートについて学びます。
+便利なライブラリ関数 trimIndent と trimMargin を使用すると、複数行のトリプルクォート文字列を周囲のコードに従ってフォーマットできます。
+trimIndent の呼び出しを、# をプレフィックス値とする trimMargin の呼び出しに置き換えて、結果の文字列にプレフィックス文字が含まれないようにします。
+
+*/
+
 const val question = "life, the universe, and everything"
 const val answer = 42
 
 val tripleQuotedString = """
     #question = "$question"
-    #answer = $answer""".trimIndent()
+    #answer = $answer""".trimMargin("#")
 
 fun main() {
     println(tripleQuotedString)
