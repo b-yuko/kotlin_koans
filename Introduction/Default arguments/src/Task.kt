4cfd0487d7e0158e70c4a317d12f85d552e7bb30
b-yuko <yuko.banno.oo@gmail.com>
2024-12-31 16:27:42 +0900

    feat: Default arguments

--- Introduction/Default arguments/src/Task.kt
+++ Introduction/Default arguments/src/Task.kt
@@ -1,4 +1,29 @@
-fun foo(name: String, number: Int, toUpperCase: Boolean) =
+/*
+
+Default arguments
+
+Javaでfoo()のオーバーロードがいくつかあると仮定します：
+
+public String foo(String name, int number, boolean toUpperCase) {
+    return (toUpperCase ? name.toUpperCase() : name) + number;
+}
+public String foo(String name, int number) {
+    return foo(name, number, false);
+}
+public String foo(String name, boolean toUpperCase) {
+    return foo(name, 42, toUpperCase);
+}
+public String foo(String name) {
+    return foo(name, 42);
+}
+
+これらのすべてのJavaのオーバーロードをKotlinの1つの関数に置き換えることができます。
+foo関数の宣言を変更し、fooを使用するコードがコンパイルされるようにしてください。
+デフォルト引数と名前付き引数を使用します。
+
+*/
+
+fun foo(name: String = "", number: Int = 42, toUpperCase: Boolean = false) =
         (if (toUpperCase) name.uppercase() else name) + number
 
 fun useFoo() = listOf(
