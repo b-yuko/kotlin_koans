32323d50ad0748ce4e6ab813ad88505461ebf74d
b-yuko <yuko.banno.oo@gmail.com>
2025-01-03 13:23:51 +0900

    feat: Nullable types

--- Introduction/Nullable types/src/Task.kt
+++ Introduction/Nullable types/src/Task.kt
@@ -1,8 +1,41 @@
+/*
+
+Nullable types
+
+Kotlin の null safety と safe calls について学び、以下の Java コードを1つの if 式のみを使って書き直してください。
+
+```java
+public void sendMessageToClient(
+    @Nullable Client client,
+    @Nullable String message,
+    @NotNull Mailer mailer
+) {
+    if (client == null || message == null) return;
+
+    PersonalInfo personalInfo = client.getPersonalInfo();
+    if (personalInfo == null) return;
+
+    String email = personalInfo.getEmail();
+    if (email == null) return;
+
+    mailer.sendMessage(email, message);
+}
+```
+
+*/
+
+
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ) {
-    TODO()
+    client ?: return
+    val personalInfo = client.personalInfo ?: return
+    val email = personalInfo.email ?: return
+    if(message.isNullOrBlank()) return
+    mailer.sendMessage(email, message)
 }
+
+// 一応テストは pass するけど、なんか違う気がする・・・。
 
 class Client(val personalInfo: PersonalInfo?)
 class PersonalInfo(val email: String?)
