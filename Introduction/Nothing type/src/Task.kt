6b51d0a55a15f000233bce50a9e081bad45d64b8
b-yuko <yuko.banno.oo@gmail.com>
2025-01-11 16:55:58 +0900

    feat: Nothing type

--- Introduction/Nothing type/src/Task.kt
+++ Introduction/Nothing type/src/Task.kt
@@ -1,6 +1,17 @@
 import java.lang.IllegalArgumentException
 
-fun failWithWrongAge(age: Int?)    {
+/*
+
+常に例外をスローする関数には、戻り値の型として Nothing を使用することができます。
+そのような関数を呼び出すと、コンパイラは関数の実行がそれ以降続かないことを前提に処理します。
+
+failWithWrongAge 関数には Nothing 型を戻り値として指定してください。
+Nothing 型を指定しない場合、checkAge 関数はコンパイルされません。
+これは、コンパイラが年齢が null である可能性を前提としているためです。
+
+*/
+
+fun failWithWrongAge(age: Int?): Nothing {
     throw IllegalArgumentException("Wrong age: $age")
 }
 
